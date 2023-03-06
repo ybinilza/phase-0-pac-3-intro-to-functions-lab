872cb33c930db117d71a5f31afc1639da4f07b2b
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-06 00:52:34 -0500

    Complete Lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -41,14 +41,16 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
   })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
+  });
+describe('sayHiToHeadphonedRoommate(string)', function() {
+    it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
   })
-
+  });
+describe('sayHiToHeadphonedRoommate(string)', function() {
   it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
     expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
       "I would love to!"
     );
   });
-})
+});
